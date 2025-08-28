2acf57b279eb4be2751230d95dfd1414bc70e4f0
FelipeFrancischini <felipe.francischini09@gmail.com>
2025-08-27 23:09:33 -0300

    Update encode_and_decode_strings.py

--- neet_solutions/array & hashing/encode_and_decode_strings.py
+++ neet_solutions/array & hashing/encode_and_decode_strings.py
@@ -1,1 +1,23 @@
 class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = ""
+        for letra in strs:
+            encoded += str(len(letra)) + "#" + letra
+        return encoded
+            
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        while i < len(s):
+            # 1. Encontra o separador #
+            j = i
+            while s[j] != "#":
+                j += 1
+            tamanho = int(s[i:j])      # número antes do #
+            # 2. Pega a substring com base no tamanho
+            palavra = s[j+1 : j+1+tamanho]
+            res.append(palavra)
+            # 3. Atualiza índice para próxima leitura
+            i = j + 1 + tamanho
+        return res
